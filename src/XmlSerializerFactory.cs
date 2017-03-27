40fb3b1bfd609e8d145680d269b007e508bd325c
Victor Milovanov <lost@losttech.software>
2017-03-26 19:46:23 -0700

    initial implementation

---
+++ src/XmlSerializerFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace LostTech.App
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    public sealed class XmlSerializerFactory : ISerializerFactory, IDeserializerFactory
+    {
+        public XmlReaderSettings ReaderSettings { get; } = new XmlReaderSettings();
+        public XmlWriterSettings WriterSettings { get; } = new XmlWriterSettings {
+            Indent = true,
+        };
+
+        public Func<Stream, Task<T>> MakeDeserializer<T>()
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var readerSettings = this.ReaderSettings.Clone();
+            return stream => {
+                var xmlReader = XmlReader.Create(stream, readerSettings);
+                return Task.FromResult((T)serializer.Deserialize(xmlReader));
+            };
+        }
+
+        public Func<Stream, T, Task> MakeSerializer<T>()
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var writerSettings = this.WriterSettings.Clone();
+            return (stream, value) => {
+                var xmlWriter = XmlWriter.Create(stream, writerSettings);
+                serializer.Serialize(stream, value);
+                return Task.CompletedTask;
+            };
+        }
+    }
+}
---
+++ src/XmlSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace LostTech.App
+{
+    using JetBrains.Annotations;
+    using PCLStorage;
+
+    public static class XmlSettings
+    {
+        public static Settings Create([NotNull] IFolder folder, IFreezerFactory freezerFactory = null)
+        {
+            freezerFactory = freezerFactory ?? ClonableFreezerFactory.Instance;
+            var serializerFactory = new XmlSerializerFactory();
+            return new Settings(folder, freezerFactory, serializerFactory, serializerFactory);
+        }
+    }
+}
